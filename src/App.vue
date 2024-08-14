495e7556103830daf5158cb32a8965398cc661d1
Bogatyrev Alexey <alexey.bogatyrev.frontend@gmail.com>
2024-08-14 07:06:35 +0500

    feat: add header

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import Header from './components/Header.vue'
+</script>
+
+<template>
+  <div class="w-4/5 m-auto h-screen bg-white rounded-xl shadow-xl">
+    <Header />
+  </div>
+</template>
---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <header class="flex justify-between items-center border-b border-slate-300 p-8">
+    <div class="flex gap-4 items-center">
+      <img src="/logo.png" alt="logo" class="w-10" />
+      <div>
+        <h2 class="text-xl font-bold uppercase">Vue Sneakers</h2>
+        <p class="text-slate-400">Магазин лучших кроссовок</p>
+      </div>
+    </div>
+    <nav>
+      <ul class="flex justify-center items-center gap-10">
+        <li class="flex items-center gap-3 text-gray-500 hover:text-black cursor-pointer">
+          <img src="/cart.svg" alt="cart" />
+          <b>1205 руб.</b>
+        </li>
+        <li class="flex items-center gap-3 text-gray-500 hover:text-black cursor-pointer">
+          <img src="/heart.svg" alt="cart" />
+          <span>Закладки</span>
+        </li>
+        <li class="flex items-center gap-3 text-gray-500 hover:text-black cursor-pointer">
+          <img src="/profile.svg" alt="cart" />
+          <span>Профиль</span>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
