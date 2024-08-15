306f2e166d929a321c39e78215682fa7b8d2c8ff
Bogatyrev Alexey <alexey.bogatyrev.frontend@gmail.com>
2024-08-15 21:33:40 +0500

    feat: add search and filter

--- src/App.vue
+++ src/App.vue
@@ -11,8 +11,26 @@ import Drawer from './components/modal/Drawer.vue'
     <Header />
 
     <div class="p-10">
-      <h2 class="text-3xl font-bold mb-8">Все кроссовки</h2>
+      <div class="flex justify-between items-center">
+        <h2 class="text-3xl font-bold mb-8">Все кроссовки</h2>
 
+        <div class="flex justify-between items-center gap-4">
+          <select class="py-2.5 px-3 border rounded-md outline-none">
+            <option value="">По названию</option>
+            <option value="">По цене (дешевые)</option>
+            <option value="">По цене (дорогие)</option>
+          </select>
+
+          <div class="relative">
+            <img src="/search.svg" alt="search" class="absolute top-3 left-4" />
+            <input
+              type="text"
+              placeholder="Поиск..."
+              class="border border-gray-200 rounded-md py-2 pl-11 pr-4 outline-none focus:border-gray-400"
+            />
+          </div>
+        </div>
+      </div>
       <CardList />
     </div>
   </div>
